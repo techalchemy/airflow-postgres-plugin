35a96eac9984f88d8f72bbdfb1b1f6ecc22d2d7e
Dan Ryan <dan@danryan.co>
2019-07-04 02:00:17 -0400

    Add bulk operator for loading postgres data
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/airflow_postgres_plugin/operators/__init__.py
+++ src/airflow_postgres_plugin/operators/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding=utf-8 -*-
 
 from .file_to_postgres_table_operator import FileToPostgresTableOperator
+from .pandas_to_postgres_bulk import PandasToPostgresBulkOperator
 from .pandas_to_postgres_operator import PandasToPostgresTableOperator
 from .postgres_table_to_file_operator import PostgresTableToFileOperator
 from .postgres_to_file_operator import PostgresToFileOperator
@@ -9,5 +10,5 @@ __all__ = [
     "FileToPostgresTableOperator",
     "PostgresTableToFileOperator",
     "PostgresToFileOperator",
-    "PandasToPostgresTableOperator",
+    "PandasToPostgresBulkOperator" "PandasToPostgresTableOperator",
 ]
--- src/airflow_postgres_plugin/operators/pandas_to_postgres_bulk.py
+++ src/airflow_postgres_plugin/operators/pandas_to_postgres_bulk.py
@@ -1,9 +1,10 @@
 # -*- coding=utf-8 -*-
+import concurrent.futures
 import contextlib
 import hashlib
 import os
 import tempfile
-from typing import IO, Any, Dict, Optional, Tuple, Union
+from typing import IO, Any, Dict, Iterator, List, Optional, Tuple, Union
 
 from airflow.exceptions import AirflowException
 from airflow.hooks.S3_hook import S3Hook
@@ -14,32 +15,30 @@ from sqlalchemy.exc import NoSuchTableError
 from airflow_postgres_plugin.hooks.postgres_hook import PostgresHook
 
 
-class PandasToPostgresTableOperator(BaseOperator):
+class PandasToPostgresBulkOperator(BaseOperator):
 
-    template_fields = ("table", "filepath", "schema", "compression")
+    template_fields = ("filepaths", "schema", "compression")
 
     @apply_defaults
     def __init__(
         self,
         conn_id: str,
-        table: str,
+        filepaths: str,
         schema: str = "public",
         sep: str = None,
         compression: str = "infer",
         chunksize: int = 10000,
-        filepath: Union[IO, str] = None,
+        max_connections: int = 10,
         s3_conn_id: str = None,
         *args,
         **kwargs,
     ):
-        super(PandasToPostgresTableOperator, self).__init__(*args, **kwargs)
+        super(PandasToPostgresBulkOperator, self).__init__(*args, **kwargs)
         self.conn_id = conn_id
-        self.table = table
         self.schema = schema
         self.sep = sep
         self.compression = compression
         self.chunksize = chunksize
-        self.filepath = filepath
         self.schema = schema
         self.s3_conn_id = s3_conn_id
         self._s3_hook: Optional[S3Hook] = None
@@ -74,28 +73,40 @@ class PandasToPostgresTableOperator(BaseOperator):
         s3_access_key = s3_credentials.secret_key
         return s3_key_id, s3_access_key
 
+    def _load_file(self, filename: str):
+        table, _, _ = os.path.basename(filename).rpartition(".csv")
+        self.log.info(
+            f"importing csv data from file: {filename} -> {table} on {self.hook!r}"
+        )
+        try:
+            result = self.hook.load_pandas(
+                table=table,
+                schema=self.schema,
+                sep=self.sep,
+                compression=self.compression,
+                chunksize=self.chunksize,
+                filepath=filename,
+            )
+        except Exception as exc:
+            raise AirflowException(f"Failed to load table with exception: {exc!r}")
+        return result
+
     def execute(self, context: Dict[str, Any]) -> Optional[str]:
+        results: List[str] = []
+        result_map: Union[Iterator, List] = []
+        filepath_list = self.filepaths.split(",")
         s3_key_id, s3_access_key = self._get_s3_credentials()
         with temp_environ():
             if s3_key_id is not None:
                 os.environ["AWS_ACCESS_KEY_ID"] = s3_key_id
             if s3_access_key is not None:
                 os.environ["AWS_SECRET_ACCESS_KEY"] = s3_access_key
-            self.log.info(
-                f"importing csv data from file: {self.filepath} on {self.hook!r}"
-            )
-            try:
-                self.hook.load_pandas(
-                    table=self.table,
-                    schema=self.schema,
-                    sep=self.sep,
-                    compression=self.compression,
-                    chunksize=self.chunksize,
-                    filepath=self.filepath,
-                )
-            except Exception as exc:
-                raise AirflowException(f"Failed to load table with exception: {exc!r}")
-        return self.table
+            with concurrent.futures.ThreadPoolExecutor(
+                max_workers=self.max_connections
+            ) as executor:
+                result_map = executor.map(self._load_file, filepath_list)
+        results = [r for r in result_map if r is not None]
+        return ",".join(results)
 
 
 @contextlib.contextmanager
@@ -104,7 +115,6 @@ def temp_environ():
     environ = dict(os.environ)
     try:
         yield
-
     finally:
         os.environ.clear()
         os.environ.update(environ)
